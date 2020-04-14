fe49f2a3b9388d9d453afffb03956e7d6bdc3978
siwen1205 <807711834@qq.com>
2020-04-14 14:12:44 +0800

    login page update

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,19 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt />
       </div>
-      <el-form>
-        <el-form-item style="margin-top: 28px">
-          <el-input></el-input>
+      <el-form :model='loginForm' :rules="loginRules" ref="loginForm" style="margin-top: 25px">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="phone number"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:200px"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:240px" placeholder="code number"></el-input>
           <el-button plain style="float: right">send code</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>agree agree agree agree agree agree</el-checkbox>
+        <el-form-item prop="checked">
+          <el-checkbox v-model="loginForm.checked">agree agree agree agree agree agree</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width: 100%">login1</el-button>
+          <el-button type="primary" style="width: 100%" @click="login">login</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -24,7 +24,45 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        checked: false
+      },
+      loginRules: {
+        mobile: [{ required: true, message: 'mobile cannot be empty' }, {
+          pattern: /^1[3456789]\d{9}$/, message: 'mobile number is not correct'
+        }],
+        code: [{ required: true, message: 'code cannot be empty' }, {
+          pattern: /^\d{6}$/, message: 'code is not correct'
+        }],
+        checked: [{ required: true }, {
+          validator: function (rull, value, callback) {
+            if (value) {
+              callback()
+            } else {
+              callback(new Error('checked the agree statement'))
+            }
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // get dom object $refs
+      this.$refs.loginForm.validate(function (isOk) {
+        if (isOk) {
+
+        }
+      })
+    }
+  }
+
+}
 </script>
 
 <style lang='less' scoped>
